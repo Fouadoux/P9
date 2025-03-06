b38874dc6ca5a0e1a9af242581301ba8498a50d1
benhadda <fouad.benhadda@gmail.com>
2025-03-06 14:21:28 +0100

    base du projet

---
+++ diabetes-risk-service/src/main/java/com/glucovision/diabetesriskservice/DiabetesRiskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.glucovision.diabetesriskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesRiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesRiskServiceApplication.class, args);
+	}
+
+}
---
+++ diabetes-risk-service/src/test/java/com/glucovision/diabetesriskservice/DiabetesRiskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glucovision.diabetesriskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesRiskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
