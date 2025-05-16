603520c767bb9aeaf7c2ff4529ad2874cb44706a
benhadda <fouad.benhadda@gmail.com>
2025-05-16 11:10:38 +0200

    test n°3 du workflow CI pour note-service

--- back/note-service/src/main/java/com/glucovision/noteservice/NoteServiceApplication.java
+++ back/note-service/src/main/java/com/glucovision/noteservice/NoteServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 
-//test ci correction du workflow
+//test n°3
 public class NoteServiceApplication {
 
 	public static void main(String[] args) {
