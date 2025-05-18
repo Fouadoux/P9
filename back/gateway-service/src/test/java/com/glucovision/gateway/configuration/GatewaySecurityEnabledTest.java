714ec6a7d33c8322d19cee8da9d28a835108d528
benhadda <fouad.benhadda@gmail.com>
2025-05-18 12:00:35 +0200

    Test n°2 build Docker image dans CI gateway-service

--- back/gateway-service/src/test/java/com/glucovision/gateway/configuration/GatewaySecurityEnabledTest.java
+++ back/gateway-service/src/test/java/com/glucovision/gateway/configuration/GatewaySecurityEnabledTest.java
@@ -2,6 +2,7 @@ package com.glucovision.gateway.configuration;
 
 import com.glucovision.gateway.security.JwtAuthenticationFilter;
 import com.glucovision.gateway.security.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewaySecurityEnabledTest {
 
@@ -23,6 +24,9 @@ class GatewaySecurityEnabledTest {
 
     @Test
     void unauthorizedWithoutToken() {
+        System.out.println("-------------GatewaySecurityEnabledTest--------------------");
+        System.out.println("Profil actif = " + System.getProperty("spring.profiles.active"));
+
         webClient.get()
                 .uri("/api/patients")
                 .exchange()
